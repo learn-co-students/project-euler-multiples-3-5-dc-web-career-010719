e80985c493b65993946dfe22802e882cbe0a3471
Dusty211 <kylebenz@hotmail.com>
2019-04-02 10:49:33 -0400

    all tests passing

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,29 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    return_a = []
+    (1...@limit).each do |i|
+      if (i % 3 == 0) || (i % 5 == 0)
+        return_a.push(i)
+      end
+    end
+    return return_a
+  end
+
+  def sum_multiples
+    return_sum = 0
+    (1...@limit).each do |i|
+      if (i % 3 == 0) || (i % 5 == 0)
+        return_sum += i
+      end
+    end
+    return return_sum
+  end
+
+end
