61cf69ea85faad5d08fdcc7d4d718c19065275cd
James Clement <jameseclement@gmail.com>
2019-05-14 14:33:31 -0400

    complete oo solution

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,12 +1,27 @@
 class Multiples
-  attr_reader :limit, :range
+  attr_reader :limit
+  attr_accessor :multiples
 
   def initialize(limit)
-    @limit = limit
-    @range = (3...limit)
+    @range = (3...limit).to_a
+    @nums = collect_multiples
   end
 
+  def collect_multiples
+    multiples = []
+    @range.each{|i|
+    if i % 3 == 0 || i % 5 == 0
+      multiples << i
+    end
+     }
+     multiples
+  end
+
+def sum_multiples
+  @nums.inject(:+)
+end
 
 
 
-end 
+
+end
